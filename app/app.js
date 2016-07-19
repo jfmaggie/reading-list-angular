de1f8c2754390fb3ce1772396cc9d5e6ab8cec5c
Meichen Zhou <jfmaggie@gmail.com>
2016-07-18 17:19:40 -0700

    Log out ui route lifecycle

--- app/app.js
+++ app/app.js
@@ -5,4 +5,27 @@ var services = require('./services/index');
 
 // Declare the app level module and dependencies
 module.exports = angular.module('readingList', ['ui.router', controllers.name, services.name])
-  .config(require('./routes'));
+  .config(require('./routes'))
+  .run(function($state, $timeout, $rootScope) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+      console.log('$stateChangeStart to ' + toState.to + '-fired when the transition begins. toState, toParams : \n', toState, toParams);
+    });
+
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams) {
+      console.log('$stateChangeError - fired when an error occurs during transition.');
+    });
+
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+      console.log('$stateChangeSuccess to ' + toState.name + '-fired once the state transition is complete.');
+    });
+
+    $rootScope.$on('$viewContentLoaded', function(event) {
+      console.log('$viewContentLoaded - fired after dom rendered', event);
+    });
+
+    $rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams) {
+      console.log('$stateNotFound ' + unfoundState.to + ' - fired when a state cannot be found by its name.');
+      console.log(unfoundState, fromState, fromParams);
+    });
+
+  });
