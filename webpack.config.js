24e0f22e20682b21414fbe991b8005f370fd6956
Christopher Yammine <christophery@mogo.ca>
2016-07-20 17:50:35 -0700

    Webpack + UIKit.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,63 @@
-var path = require('path');
+var webpack           = require('webpack');
+var path              = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var APP = __dirname + '/app/';
 
 module.exports = {
-  context: APP,
   devtool: 'source-map',
+
   entry: {
-    app: './core/bootstrap.js'
+    app: APP + 'core/bootstrap.js',
+    style: APP + 'css/reading-list.scss',
   },
+
   output: {
     path: APP,
     filename: 'bundle.js'
+  },
+
+  // what plugins we'll be using
+  // we'll also tell the ExtractTextPlugin where the final CSS file should be generated
+  // (relative to config.output.path)
+  plugins: [
+    // makes a module (JQuery here) available as variable in every module
+    // needed for UIkit
+    new webpack.ProvidePlugin({ // http://webpack.github.io/docs/shimming-modules.html
+        $: "jquery",
+        jQuery: "jquery",
+        jquery: "jquery",
+        "window.jQuery": "jquery"
+    }),
+    new ExtractTextPlugin('styles.css')
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.scss'],
+    modulesDirectories: [
+      'node_modules'
+    ]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader:  ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      },
+      {
+        test: /\.woff2(\?\S*)?$/,
+        loader: 'url-loader?limit=30000&mimetype=application/font-woff2&name=[name]-[hash].[ext]'
+      },
+      {
+        test: /\.woff(\?\S*)?$/,
+        loader: 'url-loader?limit=30000&mimetype=application/font-woff&name=[name]-[hash].[ext]'
+      },
+      {
+        test: /\.ttf(\?\S*)?$/,
+        loader: 'url-loader?limit=30000&mimetype=application/font-ttf&name=[name]-[hash].[ext]'
+      },
+    ]
   }
+
 };
